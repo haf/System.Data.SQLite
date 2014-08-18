03da2569b60ebcae26c2880a8ba09396db5d731b
mistachkin <mistachkin>
2014-08-18 20:42:22 +0000

    Preliminary changes to support having the NuGet packages use per-solution files.

--- NuGet/net40/Core/install.ps1
+++ NuGet/net40/Core/install.ps1
@@ -2,7 +2,7 @@
 #
 # install.ps1 --
 #
-# Written by Joe Mistachkin.
+# Written by Joe Mistachkin, updates by David Archer.
 # Released to the public domain, use at your own risk!
 #
 ###############################################################################
@@ -11,26 +11,47 @@ param($installPath, $toolsPath, $package, $project)
 
 $platformNames = "x86", "x64"
 $fileName = "SQLite.Interop.dll"
-$propertyName = "CopyToOutputDirectory"
+$copyToOutputDirectoryPropertyName = "CopyToOutputDirectory"
 
 foreach($platformName in $platformNames) {
-  $folder = $project.ProjectItems.Item($platformName)
+  $folder = $project.ProjectItems | where {
+    $_.Name -eq $platformName
+  }
 
   if ($folder -eq $null) {
+    $projectPath = Split-Path $project.FullName
+    $folderPath = Join-Path $projectPath $platformName
+
+    if (Test-Path $folderPath) {
+      $folder = $project.ProjectItems.AddFromDirectory($folderPath)
+
+      #
+      # NOTE: Since the EnvDTE.AddFromDirectory method is hard-wired to
+      #       recursively add an existing folder *and* all of its files,
+      #       it is necessary to remove all its items after adding.
+      #       This should be completely "safe" since the folder was just
+      #       added by this script and did not exist in the project prior
+      #       to that point.
+      #
+      foreach ($item in $folder.ProjectItems) {
+        $item.Remove()
+      }
+    } else {
+      $folder = $project.ProjectItems.AddFolder($platformName)
+    }
+  }
+
+  $item = $folder.ProjectItems | where {
+    $_.Name -eq $fileName
+  }
+
+  if ($item -ne $null) {
     continue
   }
 
-  $item = $folder.ProjectItems.Item($fileName)
+  $itemSourceDirectory = Join-Path $toolsPath $platformName
+  $itemSourceFileName = Join-Path $itemSourceDirectory $fileName
 
-  if ($item -eq $null) {
-    continue
-  }
-
-  $property = $item.Properties.Item($propertyName)
-
-  if ($property -eq $null) {
-    continue
-  }
-
-  $property.Value = 1
+  $item = $folder.ProjectItems.AddFromFile($itemSourceFileName)
+  $item.Properties.Item($copyToOutputDirectoryPropertyName).Value = 1
 }
