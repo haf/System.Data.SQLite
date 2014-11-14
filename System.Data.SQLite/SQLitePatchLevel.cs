38d9cc6b844699811aebbc117be313d43ea4c32a
mistachkin <mistachkin>
2014-11-14 01:31:56 +0000

    Reset build tags.

--- System.Data.SQLite/SQLitePatchLevel.cs
+++ System.Data.SQLite/SQLitePatchLevel.cs
@@ -9,8 +9,8 @@ using System.Data.SQLite;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-[assembly: AssemblySourceId("a0f4a5ebcf544f53e010a621d78f5316ceec10b3")]
+[assembly: AssemblySourceId(null)]
 
 ///////////////////////////////////////////////////////////////////////////////
 
-[assembly: AssemblySourceTimeStamp("2014-11-14 00:55:05 UTC")]
+[assembly: AssemblySourceTimeStamp(null)]
